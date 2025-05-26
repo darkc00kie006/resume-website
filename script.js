d3bacebf871a0d32449df613c66aadbbcb84efd8
darkc00kie006 <cjcobrador6@gmail.com>
2025-05-26 11:43:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const navLinks = document.querySelectorAll('nav ul li a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 80;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
+});
